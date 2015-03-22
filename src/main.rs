54313e723478fd6dbb251b71d19997cdc00658ab
Ivan Sagalaev <maniac@softwaremaniacs.org>
2015-03-22 01:10:45 -0700

    Initial commit.
    - Working lexer
    - Everything is still in main.rs

---
+++ src/main.rs
@@ -0,0 +1,113 @@
+#![feature(io)]
+
+extern crate ijson_rust;
+
+use std::fs::File;
+use std::io::Read;
+use std::str;
+
+const BUFSIZE: usize = 10;
+
+fn is_whitespace(value: u8) -> bool {
+    match value {
+        9 | 10 | 13 | 32 => true,
+        _ => false,
+    }
+}
+
+fn is_lexeme(value: u8) -> bool {
+    match value {
+        b'a' ... b'z' | b'0' ... b'9' |
+        b'E' |  b'.' | b'+' | b'-' => true,
+        _ => false,
+    }
+}
+
+struct Lexer<T: Read> {
+    buf: [u8; BUFSIZE],
+    len: usize,
+    pos: usize,
+    f: T,
+}
+
+impl<T: Read> Lexer<T> {
+    fn ensure_buffer(&mut self) -> bool {
+        if self.pos < self.len {
+            true
+        } else {
+            match self.f.read(&mut self.buf) {
+                Err(error) => panic!("Error reading stream: {}", error),
+                Ok(size) => { self.len = size; self.pos = 0; },
+            };
+            self.len > 0
+        }
+    }
+}
+
+impl<T: Read> Iterator for Lexer<T> {
+    type Item = Vec<u8>;
+
+    fn next(&mut self) -> Option<Vec<u8>> {
+        while self.ensure_buffer() && is_whitespace(self.buf[self.pos]) {
+            self.pos += 1;
+        }
+        if self.len == 0 {
+            return None;
+        }
+
+        let mut result = vec![];
+        if self.buf[self.pos] == b'"' {
+            result.push(b'"');
+            let mut escaped = false;
+            self.pos += 1;
+            loop {
+                let start = self.pos;
+                while self.pos < self.len && (escaped || self.buf[self.pos] != b'"') {
+                    escaped = !escaped && self.buf[self.pos] == b'\\';
+                    self.pos += 1;
+                }
+                result.extend(self.buf[start..self.pos].iter().map(|&c| c));
+                if self.pos < self.len {
+                    self.pos += 1;
+                    break;
+                } else if !self.ensure_buffer() {
+                    panic!("Unterminated string");
+                }
+            }
+            result.push(b'"');
+        } else if !is_lexeme(self.buf[self.pos]) {
+            result.push(self.buf[self.pos]);
+            self.pos += 1;
+        } else {
+            loop {
+                let start = self.pos;
+                while self.pos < self.len && is_lexeme(self.buf[self.pos]) {
+                    self.pos += 1;
+                }
+                result.extend(self.buf[start..self.pos].iter().map(|&c| c));
+                if self.pos < self.len || !self.ensure_buffer() {
+                    break;
+                }
+            }
+        }
+        Some(result)
+    }
+}
+
+fn lexer(filename: &str) -> Lexer<File> {
+    Lexer {
+        buf: [0; BUFSIZE],
+        len: 0,
+        pos: 0,
+        f: match File::open(filename) {
+            Err(error) => panic!("Can't open {}: {}", filename, error),
+            Ok(result) => result,
+        },
+    }
+}
+
+fn main() {
+    for chunk in lexer("test.json") {
+       println!("{}", str::from_utf8(&chunk).unwrap());
+    }
+}
