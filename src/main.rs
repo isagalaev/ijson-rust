225118e881b1ab9d007558f24a72ae46422ea9b5
Ivan Sagalaev <maniac@softwaremaniacs.org>
2015-05-28 22:15:49 -0700

    Single result.push with conditional logic for value.

--- src/main.rs
+++ src/main.rs
@@ -129,23 +129,25 @@ fn unescape(s: &str) -> String {
     let mut result = String::with_capacity(s.len());
     let mut chars = s.chars();
     while let Some(ch) = chars.next() {
-        if ch != '\\' {
-            result.push(ch)
-        } else {
-            match chars.next() {
-                Some('u') => {
-                    let value = chars.by_ref().take(4).fold(0, |acc, c| acc * 16 + c.to_digit(16).unwrap());
-                    result.push(char::from_u32(value).unwrap());
+        result.push(
+            if ch != '\\' {
+                ch
+            } else {
+                match chars.next() {
+                    Some('u') => {
+                        let value = chars.by_ref().take(4).fold(0, |acc, c| acc * 16 + c.to_digit(16).unwrap());
+                        char::from_u32(value).unwrap()
+                    }
+                    Some('b') => '\x08',
+                    Some('f') => '\x0c',
+                    Some('n') => '\n',
+                    Some('r') => '\r',
+                    Some('t') => '\t',
+                    Some(ch) => ch,
+                    _ => panic!("Malformed escape"),
                 }
-                Some('b') => result.push('\x08'),
-                Some('f') => result.push('\x0c'),
-                Some('n') => result.push('\n'),
-                Some('r') => result.push('\r'),
-                Some('t') => result.push('\t'),
-                Some(ch) => result.push(ch),
-                _ => panic!("Malformed escaped"),
             }
-        }
+        )
     }
     result
 }
