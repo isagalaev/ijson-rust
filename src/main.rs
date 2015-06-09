400bb5e63b8ed49e1224d4b32e9b1296a059deb5
Ivan Sagalaev <maniac@softwaremaniacs.org>
2015-06-09 16:15:23 -0700

    Benchmarking in main.rs.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
 extern crate ijson_rust;
+extern crate time;
 
 use std::fs::File;
 
 use ijson_rust::parser;
+use time::Duration;
 
 
 fn main() {
-    let f = Box::new(File::open("test.json").unwrap());
-    for (prefix, event) in parser::parse(f) {
-        println!("{}: {:?}", prefix, event);
-    }
+    let f = Box::new(File::open("load.json").unwrap());
+    let p = parser::basic_parse(f);
+    let mut count = 0;
+
+    println!("{}", Duration::span(|| {
+        for event in p {
+            if let parser::Event::String(s) = event {
+                if count == 0 {
+                    println!("{}", s);
+                }
+                count +=1
+            }
+        }
+    }));
+
+    println!("{}", count);
 }
