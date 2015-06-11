f8321db28ce89efb36be5c4958211f2af5f1744a
Ivan Sagalaev <maniac@softwaremaniacs.org>
2015-06-11 15:01:55 -0700

    Read file name from command line args.

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,18 @@ extern crate ijson_rust;
 extern crate time;
 
 use std::fs::File;
+use std::env;
 
 use ijson_rust::parser;
 use time::Duration;
 
 
 fn main() {
-    let f = Box::new(File::open("load.json").unwrap());
+    let args: Vec<_> = env::args().collect();
+    if args.len() < 2 {
+        panic!("Provide filename")
+    }
+    let f = Box::new(File::open(&args[1]).unwrap());
     let p = parser::basic_parse(f);
     let mut count = 0;
 
