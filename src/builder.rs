6c29a0432fdb447a72f51a7b9f70d89e90f9dadb
Ivan Sagalaev <maniac@softwaremaniacs.org>
2015-06-25 19:18:35 -0700

    Inlined build() as Items.next()

--- src/builder.rs
+++ src/builder.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use super::parser::{Event, Filter};
+use super::parser::Event;
 
 
 #[derive(Debug)]
@@ -16,54 +16,50 @@ pub enum Value {
 pub type Map = HashMap<String, Value>;
 pub type Array = Vec<Value>;
 
-fn build(events: &mut Filter) -> Option<Value> {
-    match events.next() {
-        None => None,
-        Some(event) => match event {
-            Event::EndMap | Event::EndArray => None,
-            Event::StartMap => {
-                let mut result = HashMap::new();
-                while let Some(event) = events.next() {
-                    match event {
-                        Event::EndMap => break,
-                        Event::Key(k) => {result.insert(k, build(events).unwrap());}
-                        _ => unreachable!(),
-                    }
-                }
-                Some(Value::Map(result))
-            }
-            Event::StartArray => {
-                let mut result = vec![];
-                while let Some(value) = build(events) {
-                    result.push(value);
-                }
-                Some(Value::Array(result))
-            }
-            Event::Null => Some(Value::Null),
-            Event::Boolean(v) => Some(Value::Boolean(v)),
-            Event::String(v) => Some(Value::String(v)),
-            Event::Number(v) => Some(Value::Number(v)),
-            Event::Key(k) => panic!("Unexpected Key event: {}", k),
-        }
-    }
+pub struct Items<I> where I: Iterator<Item=Event> {
+    events: I,
 }
 
-pub struct Items {
-    events: Filter,
-}
-
-impl Items {
-    pub fn new(events: Filter) -> Items {
+impl<I> Items<I> where I: Iterator<Item=Event> {
+    pub fn new(events: I) -> Items<I> {
         Items {
             events: events,
         }
     }
 }
 
-impl Iterator for Items {
+impl<I> Iterator for Items<I> where I: Iterator<Item=Event> {
     type Item = Value;
 
     fn next(&mut self) -> Option<Self::Item> {
-        build(&mut self.events)
+        match self.events.next() {
+            None => None,
+            Some(event) => match event {
+                Event::EndMap | Event::EndArray => None,
+                Event::StartMap => {
+                    let mut result = HashMap::new();
+                    while let Some(event) = self.events.next() {
+                        match event {
+                            Event::EndMap => break,
+                            Event::Key(k) => {result.insert(k, self.next().unwrap());}
+                            _ => unreachable!(),
+                        }
+                    }
+                    Some(Value::Map(result))
+                }
+                Event::StartArray => {
+                    let mut result = vec![];
+                    while let Some(value) = self.next() {
+                        result.push(value);
+                    }
+                    Some(Value::Array(result))
+                }
+                Event::Null => Some(Value::Null),
+                Event::Boolean(v) => Some(Value::Boolean(v)),
+                Event::String(v) => Some(Value::String(v)),
+                Event::Number(v) => Some(Value::Number(v)),
+                Event::Key(k) => panic!("Unexpected Key event: {}", k),
+            }
+        }
     }
 }
